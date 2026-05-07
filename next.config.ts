8ffcde8101717483b7ec2475f170ecbc1cdbf425
fabregaslanc3014 <fabregaslance3144@gmail.com>
2026-05-07 15:25:14 +0800

    2nd Update

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'www',
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://m.gohumano.com/apislim4lance/:path*',
-      },
-    ];
-  },
 };
 
 export default nextConfig;
